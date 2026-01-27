611819eca1d9635caad878a0d47430d606163945
David <davidguzmanvalente@gmail.com>
2026-01-26 18:06:20 -0800

    fixed player function on flipped gravity
    The player now moves correctly when the gravity is flipped onto  the roof of the map. Still haven't implemented sideways gravity but that will be for future implementations

--- scripts/gravity_button.gd
+++ scripts/gravity_button.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@onready var player = $"."
+@onready var player : CharacterBody2D = $"../Player"
 
 func _on_body_entered(_body: Node2D) -> void:
 	
@@ -8,5 +8,7 @@ func _on_body_entered(_body: Node2D) -> void:
 	print("button pressed : gravity changed")
 	if(gravity_vector == Vector2.DOWN):
 		PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.UP)
+		player.set_up_direction(Vector2.DOWN)
+		player
 	else:
 		PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,15 +5,22 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
+ 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	var gravity_vector = PhysicsServer2D.area_get_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR)
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		if(gravity_vector == Vector2.UP):
+			velocity.y = -JUMP_VELOCITY
+			animated_sprite.flip_v = true
+			print("jumped_down")
+		elif(gravity_vector == Vector2.DOWN):
+			velocity.y = JUMP_VELOCITY
+			animated_sprite.flip_v = false
+
 
 	#get the input directoin: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
