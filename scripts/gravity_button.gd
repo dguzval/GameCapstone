63459767e545ca7a6d5469ec3803b444e7f3ab31
David <davidguzmanvalente@gmail.com>
2026-01-26 20:45:13 -0800

    added an initial movable box
    the movable box is now implemented it moves abit jagedy and not smoothlike which we can fix later but overall a good start.

--- scripts/gravity_button.gd
+++ scripts/gravity_button.gd
@@ -2,13 +2,16 @@ extends Area2D
 
 @onready var player : CharacterBody2D = $"../Player"
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	
 	var gravity_vector = PhysicsServer2D.area_get_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR)
-	print("button pressed : gravity changed")
-	if(gravity_vector == Vector2.DOWN):
-		PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.UP)
-		player.set_up_direction(Vector2.DOWN)
-		player
-	else:
-		PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
+	if(body.is_in_group("player")):
+		print("button pressed : gravity changed")
+		if(gravity_vector == Vector2.DOWN):
+			PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.UP)
+			player.set_up_direction(Vector2.DOWN)
+			player.animated_sprite.flip_v = true
+		else:
+			PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
+			player.set_up_direction(Vector2.UP)
+			player.animated_sprite.flip_v = false
