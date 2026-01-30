15dc036a37d7494eecb69721dc30231d4059d076
David <davidguzmanvalente@gmail.com>
2026-01-29 18:45:24 -0800

    added gravity and updated player
    The player doesn't jump as high, gravity buttons now flip the collision shape of the player for no clipping

--- scripts/gravity_button.gd
+++ scripts/gravity_button.gd
@@ -11,7 +11,9 @@ func _on_body_entered(body: Node2D) -> void:
 			PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.UP)
 			player.set_up_direction(Vector2.DOWN)
 			player.animated_sprite.flip_v = true
+			player.collision_shape_2d.position = Vector2(0, -12.0)
 		else:
 			PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
 			player.set_up_direction(Vector2.UP)
 			player.animated_sprite.flip_v = false
+			player.collision_shape_2d.position = Vector2(0, -4.0)
