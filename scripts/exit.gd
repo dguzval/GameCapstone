e27e96d2fc17ba6b10640196faddd0b0771b67bd
David <159572097+dguzval@users.noreply.github.com>
2026-02-02 15:49:40 -0800

    updated level sequence (#9)
    The game starts at level 1 and then continues throughout all the levels once the player exits.

--- scripts/exit.gd
+++ scripts/exit.gd
@@ -1,6 +1,16 @@
 extends Area2D
 
+const FILE_BEGIN =  "res://levels/level_" 
+@onready var player : CharacterBody2D = $"../Player"
 
-func _on_body_entered(_body: Node2D) -> void:
-	print("exited level")
-	queue_free()
+func _on_body_entered(body: Node2D) -> void:
+	if(body.is_in_group("player")):
+		var current_scene_file = get_tree().current_scene.scene_file_path
+		var next_level_number = current_scene_file.to_int() + 1
+		
+		# changes the scene and updates to original gravity
+		PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
+		var next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
+		await get_tree().process_frame
+		get_tree().change_scene_to_file(next_level_path)
+		
