fb20df2f71fa8e728a30a9311ece7fd1cc3f165d
David <davidguzmanvalente@gmail.com>
2026-02-05 10:24:59 -0800

    fix: reorganized level

--- scripts/gravity_button.gd
+++ scripts/gravity_button.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var player : CharacterBody2D = $"../Player"
 
 func _on_body_entered(body: Node2D) -> void:
-	
+
 	var gravity_vector = PhysicsServer2D.area_get_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR)
 	if(body.is_in_group("player")):
 		print("button pressed : gravity changed")
