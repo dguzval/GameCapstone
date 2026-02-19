3ae9b401f77e1ef3cbce4e12a2156fc31f5ba149
David <davidguzmanvalente@gmail.com>
2026-02-18 19:28:34 -0800

    added ui
    I added the assets for the ui as well as a pause screen a main menu screen and a quick help center. These should all be implemented to each level but must be added manually

--- scripts/level_state.gd
+++ scripts/level_state.gd
@@ -10,6 +10,7 @@ var plates_exists: bool = false
 var all_plates_pressed: bool = false
 var plate_amount: int = 0
 var plates_pressed: int = 0
+var curr_level : int = 0
 
 func reset_for_level():
 	has_key = false
@@ -31,3 +32,7 @@ func check_plates_pressed():
 		all_plates_pressed = true
 	else:
 		all_plates_pressed = false
+		
+func get_level():
+	return curr_level
+	
