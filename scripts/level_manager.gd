19235906ddb5a097f24be6049f1d0cd43411e140
David <davidguzmanvalente@gmail.com>
2026-03-01 21:04:37 -0800

    added autosave and autoload

---
+++ scripts/level_manager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+const FILE_BEGIN =  "res://levels/level_" 
+var _booted := false
+
+func _ready() -> void:
+	if _booted:
+		return
+	_booted = true
+
+	print("READY:", name, " path=", get_path(), " id=", get_instance_id())
+	LevelState.load_progress()
+	print("loading...")
+
+	var scene_path = FILE_BEGIN + str(LevelState.curr_level) + ".tscn"
+	get_tree().call_deferred("change_scene_to_file", scene_path)
